b482d6e6a64e80bc0b153b43424c6237b3882b9f
John Nganga <mukunajohn329@gmail.com>
2025-01-19 23:34:27 +0300

    sqlc

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -2,22 +2,24 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"testing"
+	"time"
 
 	"github.com/flexGURU/simplebank/utils"
 	"github.com/stretchr/testify/require"
 )
 
+var ctx = context.Background()
 
-func TestCreateAccount(t *testing.T) {
 
+func createTestAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner: utils.RandomOwner(),
 		Balance: float64(utils.RandomMoney()),
 		Currency: utils.RandomCurrency(),
 	}
 
-	ctx := context.Background()
 
 	account, err := testQueries.CreateAccount(ctx, arg)
 
@@ -31,12 +33,60 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
 
+	return account
+
+}
+
+func TestCreateAccount(t *testing.T) {
+
+	createTestAccount(t)
+
+
+}
+
+func TestGetAccount(t *testing.T)  {
+
+	account1 := createTestAccount(t)
+
+	account2, err := testQueries.GetAccount(ctx, account1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.Currency, account2.Currency)
+
+	require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+
+
+	
+}
+
+func TestUpdateAccount(t *testing.T) {
+	account1 := createTestAccount(t)
+
+	arg := UpdateAccountParams{
+		ID: account1.ID,
+		Balance: float64(utils.RandomMoney()),
+	}
+
+	account2, err := testQueries.UpdateAccount(ctx, arg )
+
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+
+	fmt.Println(account2)
+
+	require.Equal(t, account1.ID, account2.ID)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account2.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.Currency, account2.Currency)
 
 
 
-
-
-
-
-
+	
 }
